546174380d7240be71fc26b24985066ee833bc4c
Christian Kolb <christian.kolb@liplex.de>
2012-04-01 23:05:13 +0200

    Forgotten file for issue #38

---
+++ system/modules/z_li_crm/dca/tl_module.php
@@ -0,0 +1,25 @@
+<?php
+if (!defined('TL_ROOT'))
+    die('You cannot access this file directly!');
+
+/**
+ * @copyright   Liplex Webprogrammierung und -design Christian Kolb 2011
+ * @author      Christian Kolb <info@liplex.de>
+ * @license     MIT (see /LICENSE.txt for further information)
+ */
+
+/**
+ * Add palettes to tl_module
+ */
+$GLOBALS['TL_DCA']['tl_module']['palettes']['registration'] = str_replace('reg_groups,','reg_groups,isCustomer,',$GLOBALS['TL_DCA']['tl_module']['palettes']['registration']);
+
+/**
+ * Add fields to tl_module
+ */
+$GLOBALS['TL_DCA']['tl_module']['fields']['isCustomer'] = array
+(
+	'label'         => &$GLOBALS['TL_LANG']['tl_module']['isCustomer'],
+	'exclude'       => true,
+	'inputType'     => 'checkbox',
+	'eval'          => array()
+);
