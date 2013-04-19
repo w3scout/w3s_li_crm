469ab6c97b6de77f070ea3719296d39e7df8452f
w3scout <info@w3scouts.com>
2013-04-19 21:15:29 +0200

    Added new terms

--- system/modules/li_crm/languages/de/li_customer.php
+++ system/modules/li_crm/languages/de/li_customer.php
@@ -1,4 +1,4 @@
-<?php if (!defined('TL_ROOT')) die('You cannot access this file directly!');
+<?php
 
 /**
  * @copyright   Liplex Webprogrammierung und -design Christian Kolb 2011
@@ -7,6 +7,12 @@
  * @license     MIT (see /LICENSE.txt for further information)
  */
 
-$GLOBALS['TL_LANG']['li_customers']['customers'] = "Kunden";
-$GLOBALS['TL_LANG']['li_customers']['customer'] = "Kunde";
-$GLOBALS['TL_LANG']['li_customers']['noEntries'] = "Keine Einträge gefunden.";
+$GLOBALS['TL_LANG']['li_customers']['customers']    = "Kunden";
+$GLOBALS['TL_LANG']['li_customers']['customer']     = "Kunde";
+$GLOBALS['TL_LANG']['li_customers']['noEntries']    = "Keine Einträge gefunden.";
+
+$GLOBALS['TL_LANG']['li_customers']['search_customer']   = "Kunde";
+$GLOBALS['TL_LANG']['li_customers']['search_project']    = "Projekt";
+$GLOBALS['TL_LANG']['li_customers']['search_service']    = "Leistung";
+$GLOBALS['TL_LANG']['li_customers']['search_product']    = "Produkt";
+?>
--- system/modules/li_crm/languages/en/li_customer.php
+++ system/modules/li_crm/languages/en/li_customer.php
@@ -1,4 +1,4 @@
-<?php if (!defined('TL_ROOT')) die('You cannot access this file directly!');
+<?php
 
 /**
  * @copyright   Liplex Webprogrammierung und -design Christian Kolb 2011
@@ -10,3 +10,8 @@
 $GLOBALS['TL_LANG']['li_customers']['customers'] = "Customers";
 $GLOBALS['TL_LANG']['li_customers']['customer'] = "Customer";
 $GLOBALS['TL_LANG']['li_customers']['noEntries'] = "No entries found.";
+
+$GLOBALS['TL_LANG']['li_customers']['search_customer']   = "Customer";
+$GLOBALS['TL_LANG']['li_customers']['search_project']    = "Project";
+$GLOBALS['TL_LANG']['li_customers']['search_service']    = "Service";
+$GLOBALS['TL_LANG']['li_customers']['search_product']    = "Product";
