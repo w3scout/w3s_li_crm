8cd01f6b03bb621a7d49b9292871d67135fc7399
w3scout <info@w3scouts.com>
2013-05-10 15:59:20 +0200

    Added new language files and terms

---
+++ system/modules/li_crm/languages/de/tl_user.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * @copyright   w3scouts.com
+ * @author      Darko Selesi <hallo@w3scouts.com>
+ * @license     MIT (see /LICENSE.txt for further information)
+ */
+
+$GLOBALS['TL_LANG']['tl_user']['licrm_invoice_legend'] = 'LiCRM';
+$GLOBALS['TL_LANG']['tl_user']['licrm_invoicep'][0] = 'Erlaubte Rechnungsfunktionen';
+$GLOBALS['TL_LANG']['tl_user']['licrm_invoicep'][1] = 'Hier können Sie die Funktionen wählen, die Sie erlauben wollen.';
+$GLOBALS['TL_LANG']['MSC']['print']                 = 'Als PDF Drucken';
+$GLOBALS['TL_LANG']['MSC']['send']                  = 'Per Email senden';
---
+++ system/modules/li_crm/languages/en/tl_user.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * @copyright   w3scouts.com
+ * @author      Darko Selesi <hallo@w3scouts.com>
+ * @license     MIT (see /LICENSE.txt for further information)
+ */
+
+$GLOBALS['TL_LANG']['tl_user']['licrm_invoice_legend'] = 'LiCRM';
+$GLOBALS['TL_LANG']['tl_user']['licrm_invoicep'][0] = 'Invoice permissions';
+$GLOBALS['TL_LANG']['tl_user']['licrm_invoicep'][1] = 'Here you can select the invoice operations you want to allow.';
+$GLOBALS['TL_LANG']['MSC']['print']                 = 'Print as PDF';
+$GLOBALS['TL_LANG']['MSC']['send']                  = 'Send via email';
